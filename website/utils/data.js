b349146638e3fb42a3345cc55bc84836d648cc22
Maria Silvia <mariasilvia907@gmail.com>
2021-09-16 21:34:38 -0300

    conexión a blognotes

--- website/utils/data.js
+++ website/utils/data.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const getAllExperiences = (callback) => {
-    request('http://localhost:3001/experiences', (error, res, body) => {
+    request('https://experienciasalta-api.herokuapp.com/experiences', (error, res, body) => {
         if(error){
           console.log('Error', error)  
           return callback(error, undefined);
@@ -13,27 +13,41 @@ const getAllExperiences = (callback) => {
             callback("No se encontraron Experiencias", undefined);
         }
     });
-}
+};
 
-// const getAllBlogNotes = (callback) => {
-//     request('http://localhost:3001/blognotes', (error, res, body) => {
-//         if(error){
-//           console.log('Error', error)  
-//           return callback(error, undefined);
-//         }
-//         if (res){
-//             if(body){
-//                return callback(undefined, body);
-//             }
-//             callback("No se encontraron Notas de Blog", undefined);
-//         }
-//     });
-// }
+const getAllBlogNotes = (callback) => {
+    request('https://experienciasalta-api.herokuapp.com/blognotes', (error, res, body) => {
+        if(error){
+          console.log('Error', error)  
+          return callback(error, undefined);
+        }
+        if (res){
+            if(body){
+               return callback(undefined, body);
+             }
+             callback("No se encontraron Notas de Blog", undefined);
+         }
+     });
+};
+
+/* const getAllMembers = (callback) => {
+    request('https://experienciasalta-api.herokuapp.com/members', (error, res, body) => {
+        if(error){
+          console.log('Error', error)  
+          return callback(error, undefined);
+        }
+        if (res){
+            if(body){
+               return callback(undefined, body);
+            }
+            callback("No se encontraron Miembros del Equipo", undefined);
+        }
+    });
+} */
 
 
 module.exports= {
-   getAllExperiences
+   getAllExperiences,
+   getAllBlogNotes,
+   //getAllMembers */
 }
-// module.exports= {
-//     getAllBlogNotes
-//  }
